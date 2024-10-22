e39fca4967f407de91a3c6b541288053eafbb558
prabeen <prabeenprabu@gmail.com>
2024-10-22 10:55:33 +0530

    reset btn added, colors added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,15 +4,15 @@ import { ColorSwatch, Group } from "@mantine/core";
 import { Button } from "../../components/ui/button";
 import axios from "axios";
 
-interface Response{
-    expr: string,
-    result: string,
-    assign: boolean,
+interface Response {
+  expr: string;
+  result: string;
+  assign: boolean;
 }
 
-interface GeneratedResult{
-    expresssion:string,
-    answer:string,
+interface GeneratedResult {
+  expresssion: string;
+  answer: string;
 }
 
 const Home = () => {
@@ -21,7 +21,7 @@ const Home = () => {
   const [color, setColor] = useState<string>(SWATCHES[0]);
   const [reset, setReset] = useState<boolean>(false);
   const [result, setResult] = useState<GeneratedResult>();
-  const [dictOfVars, setDictOfVars] = useState<{[key:string]:string}>({});
+  const [dictOfVars, setDictOfVars] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
     if (reset) {
@@ -43,16 +43,34 @@ const Home = () => {
     }
   }, []);
 
+  const sentData = async () => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const response = await axios({
+        method: "post",
+        url: "${import.meta.env.VITE_API_URL}/calculate",
+        data: {
+          image: canvas.toDataURL("image/png"),
+          dict_of_vars: dictOfVars,
+        },
+      });
+
+      const res = await response.data;
+      console.log("response: ", res);
+    }
+  };
+
   const resetCanvas = () => {
     const canvas = canvasRef.current;
 
-    if(canvas){
-        const ctx = canvas.getContext("2d");
-        if(ctx){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-        }
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
     }
-  }
+  };
 
   const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
     const canvas = canvasRef.current;
@@ -87,15 +105,46 @@ const Home = () => {
   };
 
   return (
-    <canvas
-      ref={canvasRef}
-      id="canvas"
-      className="absolute top-0 left-0 w-full h-full"
-      onMouseDown={startDrawing}
-      onMouseMove={draw}
-      onMouseOut={stopDrawing}
-      onMouseUp={stopDrawing}
-    />
+    <>
+      <Group className="z-20">
+        {SWATCHES.map((swatchColor: string) => (
+          <ColorSwatch
+            key={swatchColor}
+            color={swatchColor}
+            onClick={() => setColor(color)}
+          />
+        ))}
+      </Group>
+
+      <div className="grid grid-cols-3 gap-2">
+        <Button
+          onClick={() => setReset(true)}
+          className="z-20 bg-black text-white"
+          variant="default"
+          color="black"
+        >
+          Reset
+        </Button>
+
+        <Button
+          onClick={sentData}
+          className="z-20 bg-black text-white"
+          variant="default"
+          color="black"
+        >
+          Calculate
+        </Button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        className="absolute top-0 left-0 w-full h-full"
+        onMouseDown={startDrawing}
+        onMouseMove={draw}
+        onMouseOut={stopDrawing}
+        onMouseUp={stopDrawing}
+      />
+    </>
   );
 };
 
