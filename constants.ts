a9e3cd01bd5ece8cda38cbcff0e5bb0382f0e127
prabeen <prabeenprabu@gmail.com>
2024-10-22 10:06:24 +0530

    constant file: added colors array

---
+++ constants.ts
@@ -0,0 +1,15 @@
+const SWATCHES:string[] = [
+  "#ffffff",
+  "#ee3333",
+  "#e64980",
+  "#be4bdb",
+  "#893200",
+  "#228be6",
+  "#3333ee",
+  "#40c057",
+  "#00aa00",
+  "#fab005",
+  "#fb7e14",
+];
+
+export { SWATCHES };
